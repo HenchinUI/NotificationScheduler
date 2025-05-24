7dc9302979d72febf6e402c0ce4f9a175abca81b
HenchinUI <harjimilan4@gmail.com>
2025-05-25 04:34:57 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/notificationscheduler/MainActivity.java
+++ app/src/main/java/com/example/notificationscheduler/MainActivity.java
@@ -23,7 +23,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Initialize UI elements
         networkOptions = findViewById(R.id.networkOptions);
         mDeviceIdleSwitch = findViewById(R.id.deviceIdleSwitch);
         mDeviceChargingSwitch = findViewById(R.id.deviceChargingSwitch);
